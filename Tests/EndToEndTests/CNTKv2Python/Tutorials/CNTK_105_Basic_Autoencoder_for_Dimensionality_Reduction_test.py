Fixed momentum schedule constant negative sign error to avoid math range error; added less-than condition in 105_basic_autoencoder_for_dimensionality_reduction_simple to accommodate performance improvement because of correcting the momentum schedule logic

Tests/EndToEndTests/CNTKv2Python/Tutorials/CNTK_105_Basic_Autoencoder_for_Dimensionality_Reduction_test.py
@@ -25,4 +25,6 @@ expectedError = 3.1
 def test_cntk_105_basic_autoencoder_for_dimensionality_reduction_simple_trainerror(nb):
     testCell = [cell for cell in nb.cells
                 if cell.cell_type == 'code' and re.search('# Simple autoencoder test error', cell.source)]
-    assert np.isclose(float((testCell[0].outputs[0])['text']), expectedError, atol = TOLERANCE_ABSOLUTE)
+    assert np.isclose(float((testCell[0].outputs[0])['text']), expectedError, atol = TOLERANCE_ABSOLUTE) | \
+        np.less(float((testCell[0].outputs[0])['text']), expectedError)
+